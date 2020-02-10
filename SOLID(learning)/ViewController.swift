f154431037017133a9d355a58c6e8ff6dd5d8ce2
Damir Lutfullin <dmr.lutfullin@gmail.com>
2020-02-11 04:05:24 +0500

    Initial Commit

---
+++ SOLID(learning)/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SOLID(learning)
+//
+//  Created by Дамир Лутфуллин on 11.02.2020.
+//  Copyright © 2020 Damir Lutfullin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
